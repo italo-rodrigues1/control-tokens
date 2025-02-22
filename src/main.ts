c6dc4b12309cc0d0847fa454667eb3469a552175
italorodrigues <italosport110@hotmail.com>
2025-02-21 23:51:46 -0300

    feat: change number port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { RootModule } from './RootModule';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3344);
 }
 bootstrap();
