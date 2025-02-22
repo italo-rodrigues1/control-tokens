6d472adb121d7540f0e1e53cecd48605048ca982
italorodrigues <italosport110@hotmail.com>
2025-02-21 23:51:04 -0300

    feat: add service queue

--- src/queue/queue.controller.ts
+++ src/queue/queue.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { QueueService } from './queue.service';
 
 @Controller('queue')
 export class QueueController {
   constructor(private readonly queueService: QueueService) {}
+
+  @Post('create')
+  createQueue(@Body() payload: { queueName: string }) {
+    return this.queueService.createQueue(payload.queueName);
+  }
 }
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { QueueServiceClient } from '@azure/storage-queue';
 @Injectable()
-export class QueueService {}
+export class QueueService {
+  private readonly queueServiceClient: QueueServiceClient;
+  private readonly queueName = 'queue-control-tokens';
+  constructor() {
+    this.queueServiceClient = QueueServiceClient.fromConnectionString(
+      process.env.AZURE_CONNECT_STRING as string,
+    );
+  }
+
+  async createQueue(queueName: string): Promise<string> {
+    console.log(`Creating queue: ${queueName}`);
+    try {
+      await this.queueServiceClient.createQueue(queueName);
+      return 'Queue created successfully';
+    } catch (err) {
+      console.error('Error creating queue:', err);
+      return 'Error creating queue:' + err;
+    }
+  }
+  async addQueue(queueItem: any): Promise<void> {
+    console.log(`Adding queue: ${queueItem}`);
+    try {
+      await this.queueServiceClient
+        .getQueueClient(this.queueName)
+        .sendMessage(JSON.stringify(queueItem));
+    } catch (err) {
+      console.error('Error adding queue:', err);
+    }
+  }
+}
